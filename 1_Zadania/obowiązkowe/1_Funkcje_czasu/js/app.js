d2da091c448505eaae0a386cffade58984939a8f
Wójsik Paweł <pawel.wojsik@microsoft.wsei.edu.pl>
2019-10-06 10:18:33 +0200

    Zadanie nr 1 obowiązkowe

---
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -0,0 +1,16 @@
+function countHello(number)
+{
+    var counter = 0;
+    var interval = setInterval(() => 
+    { 
+        counter++;
+        console.log('Hello' + counter);
+        if(counter == number)
+        {
+            clearInterval(interval);
+        }
+    }, 500);
+
+}
+
+countHello(5);
